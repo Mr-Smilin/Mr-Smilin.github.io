c215dd22443a19302c980bc87cdd49be9a34595f
Smilin <lion31lion31@gmail.com>
2024-04-24 17:26:10 +0800

    Site updated: 2024-04-24 17:26:01

--- js/animation.js
+++ js/animation.js
@@ -1,66 +1,83 @@
-(function() {
-    function $() {
-        return Array.prototype.slice.call(document.querySelectorAll.apply(document, arguments));
-    }
+(function () {
+	function $(selector) {
+		if (arguments.length > 1) {
+			throw new Error("Only one argument is allowed");
+		}
 
-    $('body > .navbar, body > .section, body > .footer').forEach(element => {
-        element.style.transition = '0s';
-        element.style.opacity = '0';
-    });
-    document.querySelector('body > .navbar').style.transform = 'translateY(-100px)';
-    [
-        '.column-main > .card, .column-main > .pagination, .column-main > .post-navigation',
-        '.column-left > .card, .column-right-shadow > .card',
-        '.column-right > .card'
-    ].forEach(selector => {
-        $(selector).forEach(element => {
-            element.style.transition = '0s';
-            element.style.opacity = '0';
-            element.style.transform = 'scale(0.8)';
-            element.style.transformOrigin = 'center top';
-        });
-    });
-    
-    setTimeout(() => {
-        $('body > .navbar, body > .section, body > .footer').forEach(element => {
-            element.style.opacity = '1';
-            element.style.transition = 'opacity 0.3s ease-out, transform 0.3s ease-out';
-        });
-        document.querySelector('body > .navbar').style.transform = 'translateY(0)';
-        [
-            '.column-main > .card, .column-main > .pagination, .column-main > .post-navigation',
-            '.column-left > .card, .column-right-shadow > .card',
-            '.column-right > .card'
-        ].forEach(selector => {
-            let i = 1;
-            $(selector).forEach(element => {
-                setTimeout(() => {
-                    element.style.opacity = '1';
-                    element.style.transform = '';
-                    element.style.transition = 'opacity 0.3s ease-out, transform 0.3s ease-out';
-                }, i * 100);
-                i++;
-            });
-        });
-    });
+		const elements = document.querySelectorAll.apply(document, arguments);
+		return Array.prototype.slice.call(elements);
+	}
 
-    setTimeout(() => {
-        $('body > .navbar, body > .section, body > .footer').forEach(element => {
-            element.style = '';
-        });
-        document.querySelector('body > .navbar').style.transform = 'translateY(0)';
-        [
-            '.column-main > .card, .column-main > .pagination, .column-main > .post-navigation',
-            '.column-left > .card, .column-right-shadow > .card',
-            '.column-right > .card'
-        ].forEach(selector => {
-            let i = 1;
-            $(selector).forEach(element => {
-                setTimeout(() => {
-                    element.style = '';
-                }, i * 100);
-                i++;
-            });
-        });
-    },1000);
-}());
+	const navbar = $("body > .navbar");
+	const sections = $("body > .section");
+	const footer = $("body > .footer");
+
+	const columnMain = $(
+		".column-main > .card, .column-main > .pagination, .column-main > .post-navigation"
+	);
+	const columnLeft = $(".column-left > .card");
+	const columnRightShadow = $(".column-right-shadow > .card");
+	const columnRight = $(".column-right > .card");
+
+	const transitionNav = [...navbar, ...sections, ...footer];
+
+	const transitionElements = [
+		...columnMain,
+		...columnLeft,
+		...columnRightShadow,
+		...columnRight,
+	];
+
+	navbar.forEach((element) => {
+		element.style.transform = "translateY(-100px)";
+	});
+
+	transitionNav.forEach((element) => {
+		element.style.transition = "0s";
+		element.style.opacity = "0";
+	});
+
+	transitionElements.forEach((element) => {
+		element.style.transition = "0s";
+		element.style.opacity = "0";
+		element.style.transform = "scale(0.8)";
+		element.style.transformOrigin = "center top";
+	});
+
+	setTimeout(() => {
+		navbar.forEach((element) => {
+			element.style.transform = "translateY(0)";
+		});
+
+		transitionNav.forEach((element) => {
+			element.style.opacity = "1";
+			element.style.transition =
+				"opacity 0.3s ease-out, transform 0.3s ease-out";
+		});
+
+		transitionElements.forEach((element, index) => {
+			setTimeout(() => {
+				element.style.opacity = "1";
+				element.style.transform = "";
+				element.style.transition =
+					"opacity 0.3s ease-out, transform 0.3s ease-out";
+			}, index * 100);
+		});
+	}, 100);
+
+	setTimeout(() => {
+		navbar.forEach((element) => {
+			element.style.transform = "translateY(0)";
+		});
+
+		transitionNav.forEach((element) => {
+			element.style = "";
+		});
+
+		transitionElements.forEach((element, index) => {
+			setTimeout(() => {
+				element.style = "";
+			}, index * 100);
+		});
+	}, 1100);
+})();
